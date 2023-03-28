3f69851b41119bbd9f0d83354054700629068ff8
Bryan-clyv <117631478+Brianogango@users.noreply.github.com>
2023-03-28 21:31:10 +0300

    Revert "Ft meshack models"

--- app/serializers/payment_serializer.rb
+++
@@ -1,5 +0,0 @@
-class PaymentSerializer < ActiveModel::Serializer
-  attributes :id, :amount, :currency, :description
-  has_one :user
-  has_one :event
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  resources :payments
   resources :events
   resources :users
-  #  resource :sessions, only: [:create, :destroy]
-  post '/payments', to: 'payments#create', as: :event_payments
+#  resource :sessions, only: [:create, :destroy]
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 end
