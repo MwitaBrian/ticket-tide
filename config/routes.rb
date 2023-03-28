7f18ee4840c85016b38878ac462bfe4bee005fdb
miluche <menjokipla@gmail.com>
2023-03-28 20:43:21 +0300

    added payment route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :events
   resources :users
-#  resource :sessions, only: [:create, :destroy]
+  #  resource :sessions, only: [:create, :destroy]
+  post '/payments', to: 'payments#create', as: :event_payments
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
 end
