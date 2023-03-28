224d6800b9094a9d4120cd18bcce96fa40bd8586
miluche <menjokipla@gmail.com>
2023-03-28 21:19:53 +0300

    editing the payment controller and model

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -1,4 +1,9 @@
 class Payment < ApplicationRecord
-  belongs_to :user
-  belongs_to :event
+    belongs_to :user
+    belongs_to :event
+  
+    validates :amount, presence: true, numericality: { greater_than: 0 }
+    validates :currency, presence: true
+    validates :description, presence: true
+  end
 end
