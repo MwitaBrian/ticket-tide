4fe7af1637a7bb4037c492611326f1b42c436c17
miluche <menjokipla@gmail.com>
2023-03-29 11:10:36 +0300

    Merge changes

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,19 +1,21 @@
 class PaymentsController < ApplicationController
-        def create
-          @event = Event.find(params[:event_id])
-          @payment = @event.payments.build(payment_params)
-          @payment.user = current_user
-          if @payment.save
-            # Payment created successfully
-          else
-            # Handle payment error
-          end
-        end
-      
-        private
-      
-        def payment_params
-          params.require(:payment).permit(:amount, :currency, :description)
+   before_action :authenticate_user! 
+  
+    def create
+        @event = Event.find(params[:event_id])
+        @payment = @event.payments.build(payment_params)
+        @payment.user = current_user
+        if @payment.save
+          # Payment created successfully
+        else
+          # Handle payment error
         end
       end
+      
+      private
+      
+      def payment_params
+        params.require(:payment).permit(:amount, :currency, :description)
+      end
+    end
 end
